d34f4fada44489e8902d73828afc62b3f08d8a3f
bethperez777@gmail.com <bethperez777@gmail.com>
2024-11-05 17:42:54 -0600

    SUBIMOS LA TAREA DEL TERCER PARCIAL

---
+++ Tarea1_Parcial3_BethPérez.py
@@ -0,0 +1,62 @@
+# EJERCICIOS DE GRAFOS
+# Del .mat file de coativation_matrix, realizar los siguientes ejercicios:
+
+# 1) Crear un dataframe con la información del ndarray de coativation_matrix,
+# las columnas deben ser números desde el 1, los índices deben ser iguales a
+# las columnas.
+
+# 2) Mostrar el heatmap de coativation_matrix.
+
+# 3) Crear un dataframe subconjunto del dataframe anterior que contenga de
+# la fila 10 a la 30 y de la columna 10 a la 30.
+
+# 4) Mostrar el heatmap del nuevo dataframe.
+
+# 5) Crear el grafo dirigido del nuevo dataframe.
+
+# 6) ¿Qué nodo tiene más conexiones del nuevo dataset?
+
+# 7) Del dataframe con el array de coactivation_matrix, calcular los quantiles
+# 0.25, 0.5, 0.75 de los valores de los datos (quantiles de las conexiones).
+
+# 8) Filtra el dataframe usando como threshold los quantiles 0.25 y 0.5, ¿Cómo
+# queda la matriz de activación filtrada?
+
+# EJERCICIO DEL ARCHIVO FVE32.MAT
+
+# 1) Muestra el heatmap de la matriz de activación.
+
+# 2) Utilizando los nombres del archivo .mat, muestra el grafo en 4 formas distintas
+# (shell debe ser una de ellas).
+
+# 3) Determina el nodo con mayores conexiones.
+
+# 4) Muestra el grado solo destacando las conexiones del nodo encontrado en el
+# ejercicio anterior.
+
+# EJERCICIO
+
+# 1) Generar un número aleatorio entero en el rango [-3, 3] con seed=5002.
+
+# 2) Generar un número aleatorio flotante en el rango [-10, 10] con seed=5002.
+
+# 3) Genera un array de 1000 números con distribución normal, con mu= ej1, y
+# sigma= ej2.
+
+# 4) Plotear la distribución de los 1000 números.
+
+# 5) Calcular el valor de 1sigma, 2sigma, 3sigma.
+
+# 6) Calcular el porcentaje de números que están en el rango [-1sigma, 1sigma].
+
+# 7) Calcular el porcentaje de números que están en el rango [-2sigma, 2sigma].
+
+# 8) Calcular el porcentaje de números que están en el rango [-3sigma, 3sigma].
+
+# 9) Crear una función que reciba un entero (mu), un flotante (sigma) y un entero
+# (total de números) y: genere array de números con distribución normal, con mu,
+# y sigma. Plotee la distribución, calcule los sigmas, calcule el porcentaje de
+# números que están en los rangos de +-1sigmam +-2sigma, +-3sigma.
+
+# 10) Usar la función anterior con los valores (0, 1., 1000), (3, 5, 5000),
+# (-3, 3, 10000)
